d70560cd603afdf1dbdbf69c5302a3e4291aea12
ikmalismail17 <ikmal5a2013@gmail.com>
2024-05-20 16:30:59 +0800

    update and add route dewandisplay

--- routes/uiRequest.js
+++ routes/uiRequest.js
@@ -1,13 +1,12 @@
 import express from "express";
+import dewandisplay from "../controller/dewandisplay.js";
 
 const reqRouter = express.Router();
 
 //post request
 
 //get request
-reqRouter.get('/datadisplay', (req, res)=>{
-    res.send('Data Display');
-})
+reqRouter.get('/dewandisplay', dewandisplay)
 //put request
 
 //delete request

fd9bbbc5b983fcddca4b41b9cd23679166051567
ikmalismail17 <ikmal5a2013@gmail.com>
2024-05-28 09:16:01 +0800

    Update dewandisplay.js

--- controller/dewandisplay.js
+++ controller/dewandisplay.js
@@ -4,7 +4,26 @@ const dewanDisplay = async (req, res) => {
     let conn;
     try {
         conn = await getConnection();
-        const rows = await conn.query("SELECT * FROM dewan");
+        const rows = await conn.query(
+            `SELECT 
+                dewan.dewan_id,
+                dewan.dewan_name,
+                dewan.dewan_desc,
+                dewan.dewan_location,
+                JSON_ARRAYAGG(
+                    JSON_OBJECT(
+                        'order_id', dewan_order.order_id,
+                        'order_desc', dewan_order.order_desc,
+                        'order_explain', dewan_order.order_explain,
+                        'order_date', DATE_FORMAT(dewan_order.order_date, '%Y-%m-%d'),
+                        'dewan_order_date', DATE_FORMAT(dewan_order.dewan_order_date, '%Y-%m-%d'),
+                        'order_status', dewan_order.order_status
+                    )
+                ) AS orders
+            FROM dewan
+            JOIN dewan_order ON dewan.dewan_id = dewan_order.order_dewan_id
+            GROUP BY dewan.dewan_id;`
+        );
         res.json(rows);
     } catch (e) {
         console.error(e);

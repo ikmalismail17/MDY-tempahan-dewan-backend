28904ad8759cac92630faadec0203cf24f83510e
ikmalismail17 <ikmal5a2013@gmail.com>
2024-06-09 08:17:17 +0800

    Update uiRequest.js

--- routes/uiRequest.js
+++ routes/uiRequest.js
@@ -6,12 +6,20 @@ import dewanAdmin from "../controller/admin/dewanAdmin.js";
 const reqRouter = express.Router();
 
 //post request
+reqRouter.post('/api/tempahdewan');
+reqRouter.post('/api/dewanadd');
 
 //get request
-reqRouter.get('/dewandisplay', dewanDisplay);
-reqRouter.get('/orderdisplay', orderDisplay);
-reqRouter.get('/dewanproperty', dewanAdmin);
+reqRouter.get('/api/dewandisplay', dewanDisplay);
+reqRouter.get('/api/orderdisplay', orderDisplay);
+reqRouter.get('/api/dewanproperty', dewanAdmin);
+reqRouter.get('/api/orderdisplay/:id');
+reqRouter.get('/api/profile/:id');
+
 //put request
+reqRouter.put('/api/profile/:id');
+reqRouter.put('/api/tempahdewan/:id');
+reqRouter.put('/api/dewanupdate/:id');
 
 //delete request
 
